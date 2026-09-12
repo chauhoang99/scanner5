7a7298ee1436f9d7b646a81b1ba2a69e926dc183
chauhoang99 <40813780+chauhoang99@users.noreply.github.com>
2026-09-12 08:41:12 +0800

    Modify ticker options in app.py
    Updated ticker options to include 'XAUUSD=X' instead of 'XAUEUR=X'.

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ ticker_options = [
     "EURUSD=X", "GBPUSD=X", "AUDUSD=X", "NZDUSD=X", "USDCAD=X",
     "USDCHF=X", "USDJPY=X", "USDSGD=X", "GC=F", "BZ=F", "ZB=F",
     "BTC-USD", "EURGBP=X", "EURAUD=X", "EURNZD=X", "EURCAD=X",
-    "EURCHF=X", "EURJPY=X", "EURSGD=X", "XAUEUR=X", "GBPAUD=X",
+    "EURCHF=X", "EURJPY=X", "EURSGD=X", "XAUUSD=X", "GBPAUD=X",
     "GBPNZD=X", "GBPCAD=X", "GBPCHF=X", "GBPJPY=X", "GBPSGD=X",
     "AUDNZD=X", "AUDCAD=X", "AUDCHF=X", "AUDJPY=X", "AUDSGD=X",
     "AAPL", "MSFT", "SPY", "QQQ"
